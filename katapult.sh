6e81f1a29c7eccc93a50727d9317ae8e80b091ad
Kevin Harrington <mad.hephaestus@gmail.com>
2020-12-05 15:35:20 -0500

    adding png

--- katapult.sh
+++ katapult.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if (! test -e ~/bin) then
+ mkdir ~/bin
+fi
+if ! [ "$SCRIPT" = "$HOME/bin" ]; then
+ echo "Script source copied to ~/bin from " $SCRIPT
+ cp $SCRIPT/katapult.sh ~/bin/katapult.sh
+ chmod +x  ~/bin/katapult.sh
+fi
 
 #https://cdn.azul.com/zulu/bin/zulu8.42.0.23-ca-fx-jre8.0.232-linux_x64.tar.gz
 JVERELEASE=zulu8.44.0.13-ca-fx-jdk8.0.242-linux_x64
@@ -31,6 +41,22 @@ if (! test -e $JVMDIR/$JVERELEASE.tar.gz) then
 	rm -rf $JVMDIR
 	mkdir -p $JVMDIR
 	unzipifiy $JVERELEASE $JVMDIR
+fi
+
+if (! test -e ~/bin/katapult.desktop) then
+	echo "[Desktop Entry]
+	Version=1.0
+	Type=Katapult SmallKat Launcher
+	Comment=
+	Exec=$HOME/bin/katapult.sh
+	Icon=$HOME/bowler-workspace/gitcache/github.com/OperationSmallKat/Katapult/icon.png
+	Path=
+	Terminal=true
+	StartupNotify=false" > ~/bin/katapult.desktop
+	chmod +x ~/bin/katapult.desktop
+	gio set ~/bin/katapult.desktop "metadata::trusted" yes
+	ln -s ~/bin/katapult.desktop ~/Desktop/katapult.desktop
+	gio set ~/Desktop/katapult.desktop "metadata::trusted" yes
 fi
 
 $JVMDIR/bin/java -jar ~/bin/BowlerStudioInstall/1.12.0/BowlerStudio.jar -g https://github.com/OperationSmallKat/Katapult.git launch.groovy 
